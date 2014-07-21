06446fd5abcf475545cb767cdd39c60426f8d0be
Jonathan Doherty <jonathan@Jonathans-MacBook-Pro.local>
2014-07-21 11:27:30 +0200

    Added some index.html design TODO add more

--- seattle_theme/admin.py
+++ seattle_theme/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from mezzanine.core.admin import TabularDynamicInlineAdmin
+from mezzanine.pages.admin import PageAdmin
+#from mezzanine.core.admin import SingletonAdmin
+
 # Register your models here.
+from seattle_theme.models import (
+    HomePage,
+)
+
+class HomePageAdmin(PageAdmin):
+    pass
+
+
+admin.site.register(HomePage, HomePageAdmin)
+
+
+# class ContactMapInline(TabularDynamicInlineAdmin):
+#     model = ContactMap
+#
+#
+# class ContactPageAdmin(PageAdmin):
+#     inlines = [ContactMapInline]
+#
+#
+# admin.site.register(ContactPage, ContactPageAdmin)
