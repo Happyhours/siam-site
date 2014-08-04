301dae075d9252033350c08453c97d03034a280a
Jonathan Doherty <jonathan@Jonathans-MacBook-Pro.local>
2014-08-04 13:58:42 +0200

    Created IndexPage and added sorl-thumbnail on all but Welcome image andAdded footer contenten as sitewide singleton admin

--- seattle_theme/admin.py
+++ seattle_theme/admin.py
@@ -2,19 +2,26 @@ from django.contrib import admin
 
 from mezzanine.core.admin import TabularDynamicInlineAdmin
 from mezzanine.pages.admin import PageAdmin
-#from mezzanine.core.admin import SingletonAdmin
+from mezzanine.core.admin import SingletonAdmin
+
 
 # Register your models here.
 from seattle_theme.models import (
     HomePage,
+    IndexPage,
+    SitewideContent,
 )
 
-class HomePageAdmin(PageAdmin):
+class IndexPageAdmin(IndexAdmin):
     pass
 
+admin.site.register(IndexPage, IndexPageAdmin)
 
-admin.site.register(HomePage, HomePageAdmin)
 
+class SitewideContentAdmin(SingletonAdmin):
+    pass
+
+admin.site.register(SitewideContent, SitewideContentAdmin)
 
 # class ContactMapInline(TabularDynamicInlineAdmin):
 #     model = ContactMap
--- seattle_theme/models.py
+++ seattle_theme/models.py
@@ -9,10 +9,39 @@ from mezzanine.utils.models import upload_to
 
 class HomePage(Page, RichText):
     '''
-    A page representing the format of the home page
+    NOT USED
     '''
 
     class Meta:
-        verbose_name = _("Home page")
-        verbose_name_plural = _("Home pages")
-        
+        verbose_name = _("H page")
+        verbose_name_plural = _("H pages")
+
+
+class IndexPage(Page, RichText):
+    '''
+    A page representing the format of the home/hem/index page
+    '''
+
+    image_left = FileField(verbose_name=_("Image"),
+        upload_to=upload_to("garis_theme.HomePage.image", "feature_image"),
+        format="Image", max_length=255, null=True, blank=True)
+    image_right = FileField(verbose_name=_("Image"),
+        upload_to=upload_to("garis_theme.HomePage.image", "feature_image"),
+        format="Image", max_length=255, null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = _("Index page")
+        verbose_name_plural = _("Index pages")
+      
+
+class SitewideContent(SiteRelated):
+    '''
+    Represents the footer content
+    '''
+    box_one_content = RichTextField()
+    
+
+    class Meta:
+        verbose_name = _('Sitewide Content')
+        verbose_name_plural = _('Sitewide Content')
